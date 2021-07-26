6810a008d522cebd03d30bf6e8a04a28d1e2d99b
Justin Turner <jmturner638@gmail.com>
2021-07-26 03:06:39 -0500

    Second Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,7 +1,7 @@
 def main():
     #write your code below this line
     first = int(input("Give the first number:"))
-    second = int(input("Give the second number"))
+    second = int(input("Give the second number:"))
     sum = first + second
 
     print(str(first) + " + " + str(second) + " = " + str(sum))
