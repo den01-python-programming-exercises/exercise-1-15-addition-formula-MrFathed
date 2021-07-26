ec6cd8d11adbf624dd6118d9fd5c1eecb0e0dcc8
Justin Turner <jmturner638@gmail.com>
2021-07-26 03:05:03 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #write your code below this line
+    first = int(input("Give the first number:"))
+    second = int(input("Give the second number"))
+    sum = first + second
+
+    print(str(first) + " + " + str(second) + " = " + str(sum))
 
 if __name__ == '__main__':
     main()
